4b4188fa61a295d3b7af767e8ccf1fa1e60f741b
lazaroconceicao <joselazaroc@gmail.com>
2022-06-22 21:08:58 -0300

    Topo app

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,29 @@
 import React from "react";
-import { Text } from "react-native";
+import { StyleSheet, Image, Dimensions, Text } from "react-native";
 
-export default function Cesta () {
-    return <Text>Cesta</Text>
+import topo from "../../assets/topo.png";
+
+const width = Dimensions.get("screen").width;
+
+export default function Cesta() {
+  return<>
+    <Image source={topo} style={estilos.topo} />
+    <Text style={estilos.titulo}>Detalhes da Cesta</Text>
+  </>;
 }
+
+const estilos = StyleSheet.create({
+  topo: {
+    width: "100%",
+    height: (578 / 768) * width,
+  },
+  titulo: {
+    width: "100%",
+    position: "absolute",
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "white",
+    padding: 16,
+  }
+});
